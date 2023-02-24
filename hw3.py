ecddeb35cb1afab0c02d4bee6829e85aaa13c70d
jjoshuakkim <janggunk2001@gmail.com>
2023-02-24 12:16:59 -0600

    Think this is right

--- hw3.py
+++ hw3.py
@@ -8,42 +8,45 @@ implement expectation/maximization of 2 poisson distributions.
 Output the lambda values for each distribution
 feel free to make helper functions
 '''
+df = pd.read_csv('kmer_depth.csv')
+
+def expectation(data, m1, m2, lambda1, lambda2):
+    p1 = stats.poisson.pmf(data, m1)       # estimating the probability distribution
+    p2 = stats.poisson.pmf(data, m2)
+    w1 = lambda1 * p1 / (lambda1 * p1 + lambda2 * p2)       # Uses the prob estimates to calculate the weight for each dist using Bayes
+    w2 = lambda2 * p2 / (lambda1 * p1 + lambda2 * p2)
+    return w1, w2
+
+def maximization(data, w1, w2):
+    n = len(data)
+    lambda1 = np.sum(w1) / n                # Paramters on the poisson is updated using the weights
+    lambda2 = np.sum(w2) / n
+    mu1 = np.sum(w1 * data) / (lambda1 * n)
+    mu2 = np.sum(w2 * data) / (lambda2 * n)
+    return mu1, mu2, lambda1, lambda2
+
 def expectation_maximization(df):
     data = np.array(df['x'])
     
     # Initialize the parameters
-    mu1 = np.mean(data)
-    mu2 = np.mean(data) * 2
+    mean1 = np.mean(data)             # Means of the distribution to model data
+    mean2 = np.mean(data) * 2 
     lambda1 = 1
     lambda2 = 1
-    n = len(data)
     
-    # Implement the EM algorithm
     for i in range(100):
-        # Expectation step
-        p1 = stats.poisson.pmf(data, mu1)
-        p2 = stats.poisson.pmf(data, mu2)
-        w1 = lambda1 * p1 / (lambda1 * p1 + lambda2 * p2)
-        w2 = lambda2 * p2 / (lambda1 * p1 + lambda2 * p2)
-        
-        # Maximization step
-        lambda1 = np.sum(w1) / n
-        lambda2 = np.sum(w2) / n
-        mu1 = np.sum(w1 * data) / (lambda1 * n)
-        mu2 = np.sum(w2 * data) / (lambda2 * n)
+        w1, w2 = expectation(data, mean1, mean2, lambda1, lambda2)
+        mean1, mean2, lambda1, lambda2 = maximization(data, w1, w2)
     
-    print("lambda1 = ", lambda1)
-    print("lambda2 = ", lambda2)
+    print("lambda1 = ", mean1)
+    print("lambda2 = ", mean2)
 
-df = pd.read_csv('kmer_depth.csv')
-
-# Plot a histogram
-hist = ggplot(df, aes(x='x')) + geom_histogram(binwidth=1, fill='#0072B2', color='black') + labs(x='k-mer Depth', y='Count')
-print(hist)
-
-# Plot a density plot
-density = ggplot(df, aes(x='x')) + geom_density(fill='#0072B2', color='black') + labs(x='k-mer Depth', y='Density')
-print(density)
+def graph(list):
+    hist = ggplot(list, aes(x='x')) + geom_histogram(binwidth=1, fill='#6e0d27', color='black') + labs(x='k-mer Depth', y='Count')
+    density = ggplot(list, aes(x='x')) + geom_density(fill='#6e0d27', color='black') + labs(x='k-mer Depth', y='Density')
+    print(hist) # Histogram
+    print(density) # Density
 
 # running your EM implementation
 expectation_maximization(df)
+graph(df)
